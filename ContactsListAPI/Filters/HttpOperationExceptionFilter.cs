cf299196d64d3ab47e9a5f6e90bfa491b9ba8e0e
TheCoder87 <thecoder87@hotmail.com>
2017-05-26 20:21:21 +0530

    Initial commit

---
+++ ContactsListAPI/Filters/HttpOperationExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace ContactsList.API.Filters
+{
+    public class HttpOperationExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is HttpOperationException)
+            {
+                var ex = (HttpOperationException)context.Exception;
+                context.Response = ex.Response;
+            }
+        }
+    }
+}
---
+++ ContactsListAPI/Scripts/app/contacts.js
@@ -0,0 +1,56 @@
+﻿/* client javascript code here */
+angular.module('myApp', []).controller('contactListCtrl', function ($scope, $http) {
+
+    $scope.refresh = function () {
+        $scope.status = "Refreshing Contacts...";
+        $http({
+            method: 'GET',
+            url: '/api/contacts',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (results) {
+            $scope.contacts = results.data;
+            $scope.status = "Contacts loaded";
+        }, function (err) {
+            $scope.status = "Error loading contacts";
+        });
+    };
+
+    $scope.create = function () {
+        $scope.status = "Creating a new contact";
+
+        $http({
+            method: 'POST',
+            url: '/api/contacts',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                'Id': $scope.newId,
+                'Name': $scope.newName,
+                'EmailAddress': $scope.newEmail,
+                'CreatedBy': 'Guest'
+            }
+        }).success(function (data) {
+            $scope.status = "Contact created";
+            $scope.refresh();
+            $scope.newId = 0;
+            $scope.newName = '';
+            $scope.newEmail = '';
+        });
+    };
+
+    $scope.delete = function (id) {
+        $scope.status = "Deleting contact";
+        $http({
+            method: 'DELETE',
+            url: '/api/contacts/' + id
+        }).success(function (data) {
+            $scope.status = "Contact deleted";
+            $scope.refresh();
+        });
+    };
+
+    $scope.refresh();
+});
